1d5a111d56df18012fd020b1fea8fbe22150b985
MatiasJoaquinReynoso <matiasjoaquinreynoso@gmail.com>
2022-08-27 17:12:24 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Hello everyone, my name is Matias Reynoso (QA / TAE). I am creating this repository to start practicing with Selenium. I will apply everything learned in this Course
+
+What tools am I going to apply in this repo?
+
+- Selenium
+- Gradle
+- Cucumber
+
+Plus:
+
+- SO: Windows 10
+- IDE: VSCode
+- Test Report: junit
+- Language: Java
+- JDK: 17
+
+Contact me: matiasjoaquinreynoso@gmail.com
---
+++ app/src/test/java/steps/GoogleSteps.java
@@ -0,0 +1,25 @@
+package steps;
+
+import io.cucumber.java.en.*;
+
+public class GoogleSteps {
+    @Given("^I am on the Google search page$")
+    public void navigateToGoogle(){
+
+    }
+
+    @When("^I enter a search criteria$")
+    public void enterSearchCriteria(){
+
+    }
+
+    @And("^click on the search button$")
+    public void clickSearchButton(){
+
+    }
+
+    @Then("^the results match the criteria$")
+    public void validateResults(){
+
+    }
+}
