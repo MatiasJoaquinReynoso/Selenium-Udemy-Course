22c75c0e676446e2628a739c2d44301157b198dd
MatiasJoaquinReynoso <matiasjoaquinreynoso@gmail.com>
2022-08-28 01:17:55 -0300

    Basepage added

--- app/src/test/java/steps/GoogleSteps.java
+++ app/src/test/java/steps/GoogleSteps.java
@@ -1,11 +1,15 @@
 package steps;
 
 import io.cucumber.java.en.*;
+import pages.GooglePage;
 
 public class GoogleSteps {
+
+    GooglePage google = new GooglePage();
+
     @Given("^I am on the Google search page$")
     public void navigateToGoogle(){
-
+        google.navigateToGoogle();
     }
 
     @When("^I enter a search criteria$")
